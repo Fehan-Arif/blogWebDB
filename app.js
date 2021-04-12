28a3546a9241008ea498d9c00dabbfa1cc7016ff
Fehan Arif <fehan.arif@gmail.com>
2021-04-12 18:00:33 +1000

    Update routes and css

--- app.js
+++ app.js
@@ -1,36 +1,55 @@
 //jshint esversion:6
-
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const _ = require("lodash");
-const mongoose = require("mongoose");
-
+// 
+// Required Packages
+// 
+const express = require("express"),
+      bodyParser = require("body-parser"),
+      ejs = require("ejs"),
+      _ = require("lodash"),
+      mongoose = require("mongoose");
+// 
+// Mongoose Connect
+// 
 mongoose.connect("mongodb://localhost:27017/webDevBlogDB", { useNewUrlParser: true });
+// 
+// Generic Initial Data
+// 
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Just a basic web application on using Node.js delivering content via MongoDB.";
+const aboutContent = "Hi, I am Fehan Arif. A web Developer. I love making apps such as these for fun. Feel free to contact me about them.";
+const contactContent = "You can contact me by sending me a message below!";
+// 
+// Initialize express
+//
 
 const app = express();
+// 
+// View Settings
+//
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
+// 
+// Mongoose Schema
+//
 
 const postSchema = new mongoose.Schema({
   title: String,
   content: String
 });
+// 
+// Mongoose model
+//
 
 const Post = mongoose.model("Post", postSchema);
+// 
+// Home Route
+//
 
-
-
-
-app.get("/", function(req, res){
+app.route("/")
+.get(function(req, res){
   Post.find({}, function(err, posts){
     if (!err) {
       res.render("home", {
@@ -42,20 +61,29 @@ app.get("/", function(req, res){
     }
   })
 });
-
-app.get("/about", function(req, res){
+// 
+// About Route
+//
+app.route("/about")
+.get(function(req, res){
   res.render("about", {aboutContent: aboutContent});
 });
+// 
+// Contact Route
+//
 
-app.get("/contact", function(req, res){
+app.route("/contact")
+.get(function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
-
-app.get("/compose", function(req, res){
+// 
+// Compose Route
+//
+app.route("/compose")
+.get(function(req, res){
   res.render("compose");
-});
-
-app.post("/compose", function(req, res){
+})
+.post(function(req, res){
   const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
@@ -65,14 +93,13 @@ app.post("/compose", function(req, res){
       res.redirect("/");
     }
   });
-
-  
-
-  
-
 });
+// 
+// Posts Route
+//
 
-app.get("/posts/:postId", function(req, res){
+app.route("/posts/:postId")
+.get(function(req, res){
   const requestedTitle = _.lowerCase(req.params.postName);
   const requestedPostId = req.params.postId;
   Post.findOne({_id: requestedPostId}, function(err, post){
@@ -83,8 +110,10 @@ app.get("/posts/:postId", function(req, res){
       });
     }
   })
-
 });
+// 
+// Listen Port
+//
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
